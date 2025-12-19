b64e43536d5bb3210c0cac82e3db00bfa9861fef
Rakesh Prajapati <rakeshp8108@gmail.com>
2025-12-19 15:12:13 +0530

    Switched admin panel to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",   // ✔ CORRECT
+  baseURL: "https://portfolio-cms-backend-new.onrender.com",   // ✔ CORRECT
 });
 
 API.interceptors.request.use((req) => {
